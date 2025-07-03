bbba58f324e967e82682ffabeadf610a49640422
Proletcultist <zenin2606@gmail.com>
2025-07-03 12:22:55 +0700

    Fix new UsersFile model

--- src/main/java/ru/shift/userimporter/core/model/UsersFile.java
+++ src/main/java/ru/shift/userimporter/core/model/UsersFile.java
@@ -30,20 +30,16 @@ public class UsersFile{
 
 
 	public Integer getId(){ return id; }
-	public Integer getTotalRows(){ return totalRows; }
-	public Integer getProcessedRows(){ return processedRows; }
-	public Integer getValidRows(){ return validRows; }
-	public Integer getInvalidRows(){ return invalidRows; }
+	public Integer getInsertedRows(){ return insertedRows; }
+	public Integer getUpdatedRows(){ return updatedRows; }
 	public String getOriginalFilename(){ return originalFilename; }
 	public String getStoragePath(){ return storagePath; }
 	public String getStatus(){ return status; }
 	public String getHash(){ return hash; }
 
 	public void setId(Integer id){ this.id = id; }
-	public void setTotalRows(Integer totalRows){ this.totalRows = totalRows; }
-	public void setProcessedRows(Integer processedRows){ this.processedRows = processedRows; }
-	public void setValidRows(Integer validRows){ this.validRows = validRows; }
-	public void setInvalidRows(Integer invalidRows){ this.invalidRows = invalidRows; }
+	public void setInsertedRows(Integer insertedRows){ this.insertedRows = insertedRows; }
+	public void setUpdatedRows(Integer updatedRows){ this.updatedRows = updatedRows; }
 	public void setOriginalFilename(String originalFilename){ this.originalFilename = originalFilename; }
 	public void setStoragePath(String storagePath){ this.storagePath = storagePath; }
 	public void setStatus(String status){ this.status = status; }

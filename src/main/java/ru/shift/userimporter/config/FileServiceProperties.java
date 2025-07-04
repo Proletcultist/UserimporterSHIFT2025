d3b3629e3c83cafa51d7c8549829df077ce49f01
Proletcultist <zenin2606@gmail.com>
2025-07-04 15:23:07 +0700

    Add configuration for FileService

---
+++ src/main/java/ru/shift/userimporter/config/FileServiceProperties.java
@@ -0,0 +1,14 @@
+package ru.shift.userimporter.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.storage")
+public class FileServiceProperties{
+
+	private String location;
+
+	public String getLocation(){ return location; }
+	public void setLocation(String location){ this.location = location; }
+}

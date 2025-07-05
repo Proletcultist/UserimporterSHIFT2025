a3e6d5f5cff2f6f8d53e76fcfc9f65081993b59a
Proletcultist <zenin2606@gmail.com>
2025-07-05 22:23:37 +0700

    Add public to UsersTable and replaced CrudRepositorywith JpaRepository

--- src/main/java/ru/shift/userimporter/core/repository/UsersTable.java
+++ src/main/java/ru/shift/userimporter/core/repository/UsersTable.java
@@ -1,12 +1,12 @@
 package ru.shift.userimporter.core.repository;
 
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.shift.userimporter.core.model.User;
 
 @Repository
-interface UsersTable extends CrudRepository<User, Integer>{
+public interface UsersTable extends JpaRepository<User, Integer>{
 	User save(User user);
 
 	Optional<User> findById(Integer id);

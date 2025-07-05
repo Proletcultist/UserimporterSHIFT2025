3a0382e7409d295d27a588e28163385e4bf9194f
Proletcultist <zenin2606@gmail.com>
2025-07-05 22:21:21 +0700

    Add store method for FilesStorage

--- src/main/java/ru/shift/userimporter/core/repository/FilesStorage.java
+++ src/main/java/ru/shift/userimporter/core/repository/FilesStorage.java
@@ -1,6 +1,7 @@
 package ru.shift.userimporter.core.repository;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.shift.userimporter.config.FilesStorageProperties;
 import ru.shift.userimporter.core.exception.FilesStorageException;
+import ru.shift.userimporter.core.exception.FilesStorageBadFilenameException;
 
 @Repository
 public class FilesStorage{
@@ -25,6 +27,33 @@ public class FilesStorage{
 		}
 		catch (IOException e){
 			throw new FilesStorageException("Could not initialize storage", e);
+		}
+	}
+
+	public void store(InputStream file, String filename) throws FilesStorageException,
+	       								FilesStorageBadFilenameException{
+		Path pathedFilename;
+		try{
+			pathedFilename = Paths.get(filename);
+		}
+		catch (InvalidPathException e){
+			throw new FilesStorageBadFilenameException("Bad filename", e);
+		}
+
+
+		Path destination = rootLocation.resolve(pathedFilename)
+				.normalize().toAbsolutePath();
+
+		if (!destination.getParent().equals(rootLocation.toAbsolutePath())){
+			throw new FilesStorageBadFilenameException("Cannot store file outside appropriate directory");
+		}
+
+
+		try{
+			Files.copy(file, destination);
+		}
+		catch(IOException e){
+			throw new FilesStorageException("Failed to store file", e);
 		}
 	}
 }

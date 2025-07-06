f9f424bca2bf4fe2d2c3dbc5377d9eaedcd308d2
Proletcultist <zenin2606@gmail.com>
2025-07-06 13:50:45 +0700

    Moved storage.location property from spring property

--- src/main/java/ru/shift/userimporter/config/FilesStorageProperties.java
+++ src/main/java/ru/shift/userimporter/config/FilesStorageProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "spring.storage")
+@ConfigurationProperties(prefix = "storage")
 public class FilesStorageProperties{
 
 	private String location;

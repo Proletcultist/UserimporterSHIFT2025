08f741af0a7a28ddbe5eef11eccd5097c2aa1848
Proletcultist <zenin2606@gmail.com>
2025-07-07 19:31:29 +0700

    Add remove @Setter, add final and @RequiredArgsConstructor to UserImporterController

--- src/main/java/ru/shift/userimporter/api/contoller/UserImporterController.java
+++ src/main/java/ru/shift/userimporter/api/contoller/UserImporterController.java
@@ -8,21 +8,18 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 import ru.shift.userimporter.api.dto.PostFileResponseDto;
 import ru.shift.userimporter.core.service.FileService;
 import ru.shift.userimporter.core.model.UsersFile;
 import ru.shift.userimporter.api.mapper.UsersFileMapper;
 
 @RestController
-@Setter
+@RequiredArgsConstructor
 public class UserImporterController{
 
-	@Autowired
-	private FileService fileService;
-
-	@Autowired
-	private UsersFileMapper usersFileMapper;
+	private final FileService fileService;
+	private final UsersFileMapper usersFileMapper;
 
 	@PostMapping("/files")
 	@ResponseStatus(value = HttpStatus.CREATED)

3913ddcf7ebc2c80fec7ab05b9b1b511aad3f753
Proletcultist <zenin2606@gmail.com>
2025-07-09 00:28:19 +0700

    Make FileStatistics dto declaration prettier

--- src/main/java/ru/shift/userimporter/api/dto/FileStatistics.java
+++ src/main/java/ru/shift/userimporter/api/dto/FileStatistics.java
@@ -3,6 +3,8 @@ package ru.shift.userimporter.api.dto;
 import lombok.Builder;
 
 @Builder
-public record FileStatistics(Integer insertedLinesCount,
-				Integer updatedLinesCount,
-				Integer errorProcessedLinesCount){}
+public record FileStatistics(
+	Integer insertedLinesCount,
+	Integer updatedLinesCount,
+	Integer errorProcessedLinesCount
+){}

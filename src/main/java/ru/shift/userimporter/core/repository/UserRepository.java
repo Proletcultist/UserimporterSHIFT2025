eb190291e1e90bfa42ee69c946d66079ea0df90d
Proletcultist <zenin2606@gmail.com>
2025-07-09 22:55:41 +0700

    Add saveOrUpdate method to UserRepository

--- src/main/java/ru/shift/userimporter/core/repository/UserRepository.java
+++ src/main/java/ru/shift/userimporter/core/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package ru.shift.userimporter.core.repository;
 
-import java.time.format.DateTimeFormatter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,21 +8,19 @@ import ru.shift.userimporter.core.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
-	DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-	DateTimeFormatter timestampFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssx");
 
 	@Query(value = "INSERT INTO users" +
-		"(first_name, last_name, middle_name, email, phone, birth_date, created_at, updated_at)" + 
+		"(first_name, last_name, middle_name, email, phone, birth_date, created_at, updated_at) " + 
 		"VALUES(" +
 		":#{#user.getFirstName()}, " + 
 		":#{#user.getLastName()}, " + 
-		":#(#user.getMiddleName(), " +
+		":#{#user.getMiddleName()}, " +
 		":#{#user.getEmail()}, " +
 		":#{#user.getPhone()}, " +
-		":#{#user.getBirthDate().format(dateFormatter)}, " +
-		":#{#user.getCreatedAt().format(timestampFormatter)}, " +
-		":#{#user.getUpdatedAt().format(timestampFormatter)}, " +
-		")" +
+		":#{#user.getBirthDate()}, " +
+		":#{#user.getCreatedAt()}, " +
+		":#{#user.getUpdatedAt()}" +
+		") " +
 		"ON CONFLICT(phone) DO UPDATE SET " +
 		"first_name = EXCLUDED.first_name, " +
 		"last_name = EXCLUDED.last_name, " +

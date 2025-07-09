00de49aa900c963ae3002706fb3bbf953cae4341
Proletcultist <zenin2606@gmail.com>
2025-07-09 22:56:24 +0700

    Add updateStatus and updateRowsInfo methods to UploadedFileRepository

--- src/main/java/ru/shift/userimporter/core/repository/UploadedFileRepository.java
+++ src/main/java/ru/shift/userimporter/core/repository/UploadedFileRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.lang.Iterable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Repository;
 import ru.shift.userimporter.core.model.UsersFile;
 
@@ -13,4 +15,14 @@ public interface UploadedFileRepository extends JpaRepository<UsersFile, Long>{
 
 	@Query("SELECT f FROM UsersFile f LEFT JOIN FETCH f.errors WHERE f.status = :status")
 	List<UsersFile> findByStatusWithErrors(String status);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE UsersFile f SET f.status = :status WHERE f.id = :fileId")
+	void updateStatus(String status, long fileId);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE UsersFile f SET f.insertedRows = :insertedRows, f.updatedRows = :updatedRows WHERE f.id = :fileId")
+	void updateRowsInfo(int insertedRows, int updatedRows, long fileId);
 }
